d0f9447014e8eff8f36d57c003c0696e03e1063d
M3mentoo <3024199504@qq.com>
2026-09-01 20:40:47 +0800

    feat: add grounded player jumping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,9 +6,14 @@ namespace Tutorial.Player
     public sealed class PlayerMovement : MonoBehaviour
     {
         [SerializeField, Min(0f)] private float _moveSpeed = 6f;
+        [SerializeField, Min(0f)] private float _jumpSpeed = 9f;
+        [SerializeField] private Transform _groundCheck;
+        [SerializeField] private LayerMask _groundLayer;
+        [SerializeField, Min(0f)] private float _groundCheckRadius = 0.15f;
 
         private Rigidbody2D _rigidbody;
         private float _horizontalInput;
+        private bool _jumpRequested;
 
         private void Awake()
         {
@@ -18,6 +23,11 @@ namespace Tutorial.Player
         private void Update()
         {
             _horizontalInput = Input.GetAxisRaw("Horizontal");
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                _jumpRequested = true;
+            }
         }
 
         private void FixedUpdate()
@@ -25,6 +35,35 @@ namespace Tutorial.Player
             _rigidbody.velocity = new Vector2(
                 _horizontalInput * _moveSpeed,
                 _rigidbody.velocity.y);
+
+            if (_jumpRequested && IsGrounded())
+            {
+                _rigidbody.velocity = new Vector2(
+                    _rigidbody.velocity.x,
+                    _jumpSpeed);
+            }
+
+            _jumpRequested = false;
+        }
+
+        private bool IsGrounded()
+        {
+            return _groundCheck != null
+                && Physics2D.OverlapCircle(
+                    _groundCheck.position,
+                    _groundCheckRadius,
+                    _groundLayer) != null;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (_groundCheck == null)
+            {
+                return;
+            }
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckRadius);
         }
     }
 }
