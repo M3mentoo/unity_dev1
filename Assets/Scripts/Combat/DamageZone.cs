f66d13619b17661bb0b8b79ecad810f69c962192
M3mentoo <3024199504@qq.com>
2026-09-02 12:13:42 +0800

    feat: add reusable damage zones

---
+++ Assets/Scripts/Combat/DamageZone.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace Tutorial.Combat
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public sealed class DamageZone : MonoBehaviour
+    {
+        [SerializeField, Min(1)] private int _damage = 1;
+
+        private BoxCollider2D _collider;
+
+        private void Reset()
+        {
+            GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+
+        private void Awake()
+        {
+            _collider = GetComponent<BoxCollider2D>();
+
+            if (!_collider.isTrigger)
+            {
+                Debug.LogWarning(
+                    $"{nameof(DamageZone)} on {name} requires Is Trigger to be enabled.",
+                    this);
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            Health health = other.GetComponentInParent<Health>();
+
+            if (health != null)
+            {
+                health.TryTakeDamage(_damage);
+            }
+        }
+    }
+}
