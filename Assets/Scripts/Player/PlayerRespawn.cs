a37354b502ccf0d340d78b4b03eadd113ef8bbdf
M3mentoo <3024199504@qq.com>
2026-09-02 12:22:22 +0800

    feat: respawn player on death

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -1,18 +1,32 @@
+using Tutorial.Combat;
 using UnityEngine;
 
 namespace Tutorial.Player
 {
     [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Health))]
     public sealed class PlayerRespawn : MonoBehaviour
     {
         [SerializeField] private Transform _respawnPoint;
         [SerializeField] private float _fallThreshold = -8f;
 
         private Rigidbody2D _rigidbody;
+        private Health _health;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _health = GetComponent<Health>();
+        }
+
+        private void OnEnable()
+        {
+            _health.Died += Respawn;
+        }
+
+        private void OnDisable()
+        {
+            _health.Died -= Respawn;
         }
 
         private void FixedUpdate()
@@ -22,9 +36,20 @@ namespace Tutorial.Player
                 return;
             }
 
+            Respawn();
+        }
+
+        private void Respawn()
+        {
+            if (_respawnPoint == null)
+            {
+                return;
+            }
+
             _rigidbody.velocity = Vector2.zero;
             _rigidbody.angularVelocity = 0f;
             _rigidbody.position = _respawnPoint.position;
+            _health.RestoreFullHealth();
         }
     }
 }
